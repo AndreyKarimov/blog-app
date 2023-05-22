6237f97bedcf4aee9209476941ce352802fd3ee6
Andrey Karimov <AndreyKarimov@gmail.com>
2023-05-23 01:22:18 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,73 @@
+// --> elements from HTML
+const postTitleNode = document.getElementById("js-postTitle");
+const postContentNode = document.getElementById("js-postContent");
+const postPublishNode = document.getElementById("js-postPublish");
+const postListNode = document.getElementById("js-postList");
+const postItemDateNode = document.getElementById("js-postItemDate");
+const postItemContentNode = document.getElementById("js-postItemContent");
+const foolProofNode = document.getElementById("js-foolProof");
+
+// --> constants
+
+// --> variables
+const posts = [];
+
+// --> main programme
+postPublishNode.addEventListener("click", function () {
+  const postFromUser = getPostFromUser();
+  setPost(posts, postFromUser);
+  renderPosts(posts);
+});
+
+// --> functions
+
+//get value from field
+const getValue = (Node) => {
+  return Node.value;
+};
+
+//get post from user
+const getPostFromUser = (title, content, date) => {
+  title = getValue(postTitleNode);
+  content = getValue(postContentNode);
+  date = new Date();
+  postTitleNode.value = "";
+  postContentNode.value = "";
+  return {
+    title: title,
+    content: content,
+    date: date,
+  };
+};
+
+//set post in posts
+const setPost = (posts, post) => {
+  posts.push(post);
+};
+
+//render posts in post-list
+const renderPosts = (posts) => {
+  postListNode.innerHTML = "";
+  posts.forEach((post) => {
+    const postListItem = document.createElement("li");
+    postListItem.classList.add("post-item");
+
+    const postDate = document.createElement("div");
+    postDate.classList.add("post-item__date");
+    postDate.innerText = post.date.toLocaleString();
+
+    const postTitle = document.createElement("div");
+    postTitle.classList.add("post-item__title");
+    postTitle.innerText = post.title;
+
+    const postContent = document.createElement("div");
+    postContent.classList.add("post-item__content");
+    postContent.innerText = post.content;
+
+    postListItem.appendChild(postDate);
+    postListItem.appendChild(postTitle);
+    postListItem.appendChild(postContent);
+
+    postListNode.appendChild(postListItem);
+  });
+};
